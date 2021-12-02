2bbad98d643e65f29fcc20bab1fd9a97e2df92ed
SilviaCat <silvia.catte@hotmail.it>
2021-12-02 19:44:51 +0100

    Delete app/js directory

--- app/js/script.js
+++
@@ -1,9 +0,0 @@
-const boxHover = document.querySelector(".card__image");
-const view = document.querySelector(".image__view");
-
-boxHover.addEventListener("mouseenter", function() {
-    view.classList.add('vedi');
-});
-boxHover.addEventListener("mouseleave", function() {
-    view.classList.remove('vedi');
-});
